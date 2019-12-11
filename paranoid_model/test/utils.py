d7868d0da0d39a17bb22aee57bd1dff9acb7b684
luan rodrigues <luanrodrigues007@hotmail.com>
2019-12-11 09:13:10 -0300

    fix import bug

--- paranoid_model/test/utils.py
+++ paranoid_model/test/utils.py
@@ -1,6 +1,11 @@
+from faker import Faker
+from paranoid_model.models import Person
 """
 File wiht utils functions
 """
+
+
+fake = Faker('en_US')
 
 
 def isdeleted(obj):
@@ -36,3 +41,30 @@ def any_list(iterable, key):
         bool(): if any objects pass the key
     """
     return any([key(item) for item in iterable])
+
+
+def get_person_instance():
+        """
+        Method to return an instance of a Person
+        not saved on database
+        Returns:
+            Person()
+        """
+        return Person(name=fake.prefix() + fake.name())
+
+
+def create_list_of_person(quantity_saved, quantity_deleted):
+        """
+        Method to create a list of person on database
+        Args:
+            quantity_saved: int with amount of instances to save
+            quantity_deleted: int with amount of instances to save and then delete
+        """
+        for count in range(quantity_saved):
+            person = get_person_instance()
+            person.save()
+
+        for count in range(quantity_deleted):
+            person = get_person_instance()
+            person.save()
+            person.delete()
