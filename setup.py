ca6aaf130b65e26af778cdef8673c1987a72fb8f
Luan Rodrigues <luanrodrigues007@hotmail.com>
2019-12-14 21:12:01 -0300

    end setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,39 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="django-paranoid-model", # Replace with your own username
-    version="0.0.1",
-    author="Luan Rodrigues",
-    author_email="luanrodriguesbusiness@hotmail.com",
-    description="Django abstract model with paranoid behavior,",
+    name='django-paranoid-model', # Replace with your own username
+    version='0.0.1',
+    url='https://github.com/DarknessRdg/django-paranoid-model',
+    keywords='django paranoid safedelete softdelete',
+
+    author='Luan Rodrigues',
+    author_email='luanrodriguesbusiness@hotmail.com',
+    
+    description='Django abstract model with paranoid behavior,',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/DarknessRdg/django-paranoid-model",
+    long_description_content_type='text/markdown',
+    
     packages=setuptools.find_packages(),
+    
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Development Status :: 4 - Beta Test',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
+
+    install_requires=['Django'],
+    extra_requires={
+        'test': ['Faker']
+    },
     python_requires='>=3.4',
+
+    project_urls={
+        'Bug Reports': 'https://github.com/DarknessRdg/django-paranoid-model/issues',
+        'Source': 'https://github.com/DarknessRdg/django-paranoid-model/'
+    }
 )
